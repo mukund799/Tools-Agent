7e922b7f898a72eae10b1276258b5378bb0aa311
MUKUND NARAYAN <mukundnarayan@MUKUNDs-MacBook-Air.local>
2024-10-07 21:50:31 +0530

    addedd history also

--- agent.py
+++ agent.py
@@ -3,13 +3,19 @@ from langchain_core.prompts import ChatPromptTemplate
 from tools import tools
 from llm import load_llm
 llm = load_llm()
+
+# The agent prompt must have an agent_scratchpad key that is a
+# MessagesPlaceholder. Intermediate agent actions and tool output messages will be passed in here.
+
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "You can only respond using the provided tools. if you can't find answer using the tools, then only response back to user that 'Sorry, I cannot answer that.'\n"),
+        ("placeholder", "{chat_history}"),
         ("human", "{input}"),
         # Placeholders fill up a **list** of messages
-        ("placeholder", "{agent_scratchpad}")
+        ("placeholder", "{agent_scratchpad}")      
     ]
 )
+
 agent = create_tool_calling_agent(llm,tools,prompt)
-agent_executor = AgentExecutor(agent=agent,tools=tools,handle_parsing_errors=True, return_intermediate_steps=True)
+agent_executor = AgentExecutor(agent=agent,tools=tools,handle_parsing_errors=True, ) #return_intermediate_steps=True
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from agent import agent_executor
+from langchain_core.messages import AIMessage, HumanMessage
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+store = {}
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
 
 def main():
     while(True):
@@ -6,8 +24,12 @@ def main():
         if question.lower() == "exit":
             print("Happy to end!!")
             break
-        res = agent_executor.invoke({"input":question})
-        print(res['output'])
+        # res = agent_executor.invoke({"input":question})
+        res = agent_with_chat_history.invoke(
+                {"input": question},
+                config={"configurable": {"session_id": "<foo>"}},
+            )
         print(res)
+        
 if __name__ == "__main__":
     main()
